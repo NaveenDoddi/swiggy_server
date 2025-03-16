755dbdbc621ea8435f2ff2f882bc76bbda8a8f03
naveendoddi <prasadnaveen847@gmail.com>
2025-03-16 15:58:28 +0530

    server.js

--- server.js
+++ server.js
@@ -253,7 +253,4 @@ app.get("/", (req, res) => {
       res.send("Hello from Vercel!");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
 module.exports = app;
